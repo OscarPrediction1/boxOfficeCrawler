b4ac11c8ba520726562b755f59995712bf7425bb
Thomas Brüggemann <mail@thomasbrueggemann.com>
2015-10-20 22:48:09 +0200

    reading gross domestic gross per movie

--- crawl.py
+++ crawl.py
@@ -1,4 +1,5 @@
-import json
+import json, requests
+from bs4 import BeautifulSoup
 
 # open movies file with box office ids
 with open("movies.json", "r") as movies_file:
@@ -11,4 +12,16 @@ for movie in movies:
 
 	# construct box office url
 	url = "http://www.boxofficemojo.com/movies/?id=" + movie["boxOfficeId"] +".htm"
-	print url
+	r = requests.get(url)
+
+	# load html file into parser
+	soup = BeautifulSoup(r.text, "html.parser")
+
+	# loop all font elements
+	for font_el in soup.find_all("font"):
+
+		# test
+		if "Domestic Total Gross:" in font_el.get_text():
+
+			movie["domesticTotalGross"] = int(font_el.get_text().replace("Domestic Total Gross: $", "").replace(",", ""))
+			print movie["name"] + ": $" + str(movie["domesticTotalGross"])
